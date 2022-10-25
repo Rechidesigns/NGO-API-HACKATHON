85d8a611bdd70dcb1fbf1f6614fc537e14a17797
Rechidesigns <alerechiordu@gmail.com>
2022-10-25 01:57:01 +0100

    data base settings

--- ngo_project_folder/settings.py
+++ ngo_project_folder/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ngohackathonapp.herokuapp.com', '127.0.0.1' ]
 
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'ngo_project_folder.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-}
+# }
 
 
 
