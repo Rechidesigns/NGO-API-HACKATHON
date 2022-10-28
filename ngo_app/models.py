e36de22b2f83bbd5bd160b73643a5dd0776437e7
Rechidesigns <alerechiordu@gmail.com>
2022-10-28 21:50:31 +0100

    models upgrade

--- ngo_app/models.py
+++ ngo_app/models.py
@@ -3,15 +3,20 @@ from django.utils import timezone
 from django.forms import model_to_dict
 from phonenumber_field.modelfields import PhoneNumberField
 
-
+STATUS = (("unpublished","Unpublished"), 
+              ("published","Published"))
+    
 
 class Ngo(models.Model):
     name = models.CharField(max_length=100)
-    reg_num = models.TextField(max_length=200, default = False)
+    description = models.TextField(blank=True, null=True)
+    reg_num = models.TextField(max_length=200, blank=True, null=True)
     address = models.TextField(max_length=200)
     phone = PhoneNumberField()
+    email = models.EmailField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
-    
+
     def _str_(self):
         return self.name
 
@@ -24,8 +29,13 @@ class Ngo(models.Model):
 class Blog(models.Model):
     tittle = models.CharField(max_length=250)
     details = models.TextField()
-    image = models.ImageField(default = False)
+    image = models.ImageField(blank=True, null=True)
     date_posted = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=250, blank=True, null=True)
+    status = models.CharField(max_length=50, choices=STATUS, default="published")
+
+    STATUS = (("unpublished","Unpublished"), 
+              ("published","Published"))
 
     def _str_(self):
         return self.tittle
